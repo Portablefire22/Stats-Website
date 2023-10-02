a3f8d84676fdd46a2b15307ad8ca263508696b71
Portablefire22 <barnowlsarecool@gmail.com>
2023-10-02 23:40:40 +0100

    Server now gets the summoner's profile as specified by the url. Displays the information received in plain text

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::env;
+use dotenvy::dotenv;
+use syn;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
 
 #[macro_use] extern crate rocket;
 
@@ -8,13 +12,36 @@ fn index() -> &'static str{
 }
 
 #[get("/<region>/<username>")]
-fn userProfile(region: &str, username: &str) -> String{
-    format!("Region: {}\nUsername: {}", region, username)
+async fn userProfile(region: &str, username: &str) -> String{
+
+    format!("Region: {}\nUsername: {}", region, username);
+    let riot_api: String = (env::var("RIOT_API").unwrap()).replace('"', "");
+    println!("{}", riot_api);
+    let request_url: String = format!("https://{}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}?api_key={}",region, username, &riot_api);
+    let resp = reqwest::get(request_url).await.expect("Failed to get a response").text().await.expect("Could not parse");
+    let model: Summoner = serde_json::from_str(&resp).unwrap();
+    format!("{:#?}", model)
 }
 
 #[launch]
 fn rocket() -> _ {
+    dotenv().expect(".env file not found");
     rocket::build()
         .mount("/", routes![index])
         .mount("/search/", routes![userProfile])
+
+}
+
+
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Summoner {
+    id: String,
+    account_id: String,
+    puuid: String,
+    name: String,
+    profile_icon_id: i64,
+    revision_date: i64,
+    summoner_level: i64,
 }
