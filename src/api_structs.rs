e6102579f15d3ca8ee9632509506cef53c428447
Portablefire22 <barnowlsarecool@gmail.com>
2023-10-03 17:18:44 +0100

    Structs from the api are moved into a separate file for easier use in the future and to not clutter the main file.
    Raw ranked information from the api can be collected by searching with the right url.
    
    Serious commit messages from now on. :3

---
+++ src/api_structs.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SummonerInfo {
+    pub id: String,
+    pub account_id: String,
+    pub puuid: String,
+    pub name: String,
+    pub profile_icon_id: i64,
+    pub revision_date: i64,
+    pub summoner_level: i64,
+}
+
+pub struct Summoner {
+    pub summoner_info: SummonerInfo,
+    pub region: String,
+}
+pub type SummonerRanked = Vec<SummonerRankedElement>;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SummonerRankedElement {
+    pub league_id: String,
+    pub queue_type: String,
+    pub tier: String,
+    pub rank: String,
+    pub summoner_id: String,
+    pub summoner_name: String,
+    pub league_points: i64,
+    pub wins: i64,
+    pub losses: i64,
+    pub veteran: bool,
+    pub inactive: bool,
+    pub fresh_blood: bool,
+    pub hot_streak: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use std::env;
 use dotenvy::dotenv;
 use syn;
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
 
+
+mod api_structs;
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -19,29 +20,29 @@ fn index() -> &'static str{
 // Lets focus on the back end
 // No point in making an Error 500 look pretty
 #[get("/<region>/<username>")]
-async fn user_profile(region: &str, username: &str) -> &'static str {
+async fn user_profile(region: &str, username: &str) -> String {
     format!("Region: {}\nUsername: {}", region, username);
     let riot_api: String = (env::var("RIOT_API").unwrap()).replace('"', "");
     println!("{}", riot_api);
     let request_url: String = format!("https://{}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}?api_key={}",region, username, &riot_api);
     let resp = reqwest::get(request_url).await.expect("Failed to get a response").text().await.expect("Could not parse");
-    let mut local_summoner_info: SummonerInfo = serde_json::from_str(&resp).unwrap();
-    let local_summoner: Summoner = Summoner {
+    let mut local_summoner_info: api_structs::SummonerInfo = serde_json::from_str(&resp).unwrap();
+    let local_summoner: api_structs::Summoner = api_structs::Summoner {
         summoner_info: local_summoner_info,
         region: region.parse().unwrap(),
     };
-    testing_function(&local_summoner).await;
-    return "String" // Returns the profile as a Summoner struct
+    format!("{:#?}", get_ranked_information(&local_summoner).await) // Returns the profile as a Summoner struct
 }
 
-async fn testing_function(local_summoner: &Summoner) {
+async fn get_ranked_information(local_summoner: &api_structs::Summoner) -> api_structs::SummonerRanked{
     let riot_api: String = (env::var("RIOT_API").unwrap()).replace('"', "");
     let request_url: String = format!("https://{}.api.riotgames.com/lol/league/v4/entries/by-summoner/{}?api_key={}", local_summoner.region, local_summoner.summoner_info.id, riot_api);
     println!("{:#?}", request_url);
     let mut resp = reqwest::get(request_url).await.expect("Failed to get a response").text().await.expect("Could not parse");
-    println!("{:#?}", resp[0]); // Im eepy sleep, i leave this to future me
-    resp = serde_json::from_str(&resp[0]).unwrap();
     println!("{}", resp);
+    let local_summoner_ranked: api_structs::SummonerRanked = serde_json::from_str(&resp).unwrap();
+    println!("{:#?}", local_summoner_ranked);
+    local_summoner_ranked
 }
 
 #[launch]
@@ -55,19 +56,3 @@ fn rocket() -> _ {
 
 
 
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct SummonerInfo {
-    id: String,
-    account_id: String,
-    puuid: String,
-    name: String,
-    profile_icon_id: i64,
-    revision_date: i64,
-    summoner_level: i64,
-}
-
-pub struct Summoner {
-    summoner_info: SummonerInfo,
-    region: String,
-}
